f65538b93217cb39ae408f74aaadc2ae767fe829
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-06-02 21:20:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const app = "I don't do much."
+// const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendKitten(kittens, name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(kittens, name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(kittens) {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyAddLastKitten (kittens) {
+  kittens.shift();
+  return kittens;
+}
+
+//
+// function appendKitten()
